928aa6735f131b9ca0187d2865fa04244df1b991
peace <wjppeace@163.com>
2026-03-03 15:48:47 +0800

    init - 初始化项目

---
+++ witkey-api/src/main/java/com/witkey/api/WitkeyApiApplication.java
@@ -0,0 +1,13 @@
+package com.witkey.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitkeyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitkeyApiApplication.class, args);
+    }
+
+}
