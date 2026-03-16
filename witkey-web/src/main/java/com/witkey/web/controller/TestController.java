07ff21d4b5ea9363c2362311824db7728b08169e
peace <wjppeace@163.com>
2026-03-16 17:00:17 +0800

    add - 初步整合Spring Security框架

--- witkey-web/src/main/java/com/witkey/web/controller/TestController.java
+++ witkey-web/src/main/java/com/witkey/web/controller/TestController.java
@@ -24,7 +24,7 @@ import java.time.LocalTime;
 @Slf4j
 public class TestController {
 
-    @PostMapping("/test")
+    @PostMapping("/admin/test")
     @ApiOperationLog(description = "测试接口")
     public Response test(@RequestBody @Validated User user) {
         // 打印入参
